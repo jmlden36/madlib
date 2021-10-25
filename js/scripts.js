5946add8bc2a041e04302ef80d812ce6d93a47b7
Matt Luker <jamesmattluker@gmail.com>
2021-10-25 12:34:31 -0600

    create inputArray, outputArray, and indexArray and exclude previous commented out code to use looping

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,25 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
+    const inputArray = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
+    const outputArray = [".person1", ".person2", ".animal", ".exclamation", ".verb", ".noun"]
+    let arrayIndex = 0;
+
+    inputArray.forEach(function(element) {
+      $(outputArray[arrayIndex]).text(element);
+      arrayIndex += 1;
+
+
+
+    });
+/*    
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
-
+*/
     $("#story").show();
   });
 });
